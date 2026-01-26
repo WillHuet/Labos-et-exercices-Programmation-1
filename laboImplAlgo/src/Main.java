7ae3936c6f9833c6d9598994ea617117a0c5034a
William Huet <willhuet02@gmail.com>
2026-01-26 09:30:44 -0500

    INIT Ajout du labo au dossier
    Ajout du code dans le folder. Labo déjà débuté. (fonctionne déjà à 80%, restes de la finition)

---
+++ laboImplAlgo/src/Main.java
@@ -0,0 +1,65 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    int NBR_SECONDES_JOUR = 86400;
+    int NBR_SECONDES_HEURE = 3600;
+    int NBR_SECONDES_MINUTE = 60;
+    String MSG_PREMIER_NBR = "Entrez un nombre de secondes! Nous allons convertir le tout en jours, heures, minutes et secondes:";
+    String MSG_NOUVEAU_NBR = "Entrez un nouveau nombre de secondes! Nous allons convertir le tout en jours, heures, minutes et secondes:";
+    String MSG_ERREUR_NBR = "Nombre de secondes rentré est négatif ou nul! Réessayez avec un nombre plus grand que 0";
+
+
+    int nbrSecondesInput;
+    int nbrSecondes;
+    int secondes;
+    int minutes;
+    int heures;
+    int jours;
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println(MSG_PREMIER_NBR);
+    nbrSecondesInput = scanner.nextInt();
+
+    while(nbrSecondesInput != 0){
+        nbrSecondes = nbrSecondesInput;
+        secondes = 0;
+        minutes = 0;
+        heures = 0;
+        jours = 0;
+        if (nbrSecondes > 0){
+            while(nbrSecondes > 0){
+                if(nbrSecondes >= NBR_SECONDES_JOUR){
+                    jours++;
+                    nbrSecondes = nbrSecondes - NBR_SECONDES_JOUR;
+                }
+                else if(nbrSecondes >= NBR_SECONDES_HEURE){
+                    heures++;
+                    nbrSecondes = nbrSecondes - NBR_SECONDES_HEURE;
+                }
+                else if(nbrSecondes >= NBR_SECONDES_MINUTE){
+                    minutes++;
+                    nbrSecondes = nbrSecondes - NBR_SECONDES_MINUTE;
+                }
+                else{
+                    secondes = nbrSecondes;
+                    nbrSecondes = 0;
+                }
+            }
+            System.out.println("("+ jours +","+ heures +","+ minutes +","+ secondes +")");
+            System.out.println(MSG_NOUVEAU_NBR);
+            nbrSecondesInput = scanner.nextInt();
+
+        }
+        else {
+            while(nbrSecondes < 0){
+                System.out.println(MSG_ERREUR_NBR);
+                nbrSecondes = scanner.nextInt();
+            }
+        }
+    }
+
+    //for (int i = 1; i <= 5; i++) {
+        //IO.println("i = " + i);
+    //}
+}
