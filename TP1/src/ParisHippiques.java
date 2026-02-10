0977ca5e097dea262b18c823e4e746f8bcd9e4ef
William Huet <willhuet02@gmail.com>
2026-02-10 10:46:56 -0500

    INIT & CREATE Projet «TP1»
    Initiation et création du projet "TP1". Importation de la librairie "Clavier" ainsi que de la classe "TP1Utils" pour avoir accès à la méthode "executerCourse()".

---
+++ TP1/src/ParisHippiques.java
@@ -0,0 +1,14 @@
+/**
+ * Cette classe contient...
+ *
+ * @author : William Huet (HUEW75120205)
+ * @version : 10 février 2026
+ */
+
+public class ParisHippiques {
+    //CONSTANTES
+    public static void main(String[] args) {
+        //VARIABLES
+        System.out.print("Hello and welcome!");
+    }
+}
