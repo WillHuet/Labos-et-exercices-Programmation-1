9018bbbb62d6a7efa5aa40daf2c99161e115ec9c
William Huet <willhuet02@gmail.com>
2026-03-17 11:48:17 -0400

    FCT Validation clé terminée
    Méthode de validation pour la clé de cryptage terminée (commentaire aussi).

--- TP2/TP2/src/Methodes.java
+++ TP2/TP2/src/Methodes.java
@@ -16,37 +16,72 @@ public class Methodes {
 //        }
     }
 
-    public static void validerCle(String texte) {
+    /**
+     * Cette méthode valide si la clé de cryptage est valide.
+     * La validation s'effectue sur plusieurs étapes :
+     *  1) Valider le nombre de caractères (doit être divisible par 4).
+     *  2) Diviser notre clé en blocs de quatre caractères.
+     *  3) Valider si les deux premiers caractères du bloc sont valides (RG, RD, PE, PI, ou IV).
+     *  4) Valider si les deux derniers caractères du bloc sont des chiffres et s'ils sont valides (entre 00 et 99).
+     *
+     * Ces quatre étapes s'effectuent autant de fois qu'il y a de bloc de quatre caractères.
+     * Si aucune erreur n'a été repéré au cours de l'exécution de la boucle, le programme continue.
+     *
+     * @param cle la clé de cryptage
+     */
+    public static void validerCle(String cle) {
         boolean valide = false;
-        boolean validationLettres = false;
-        boolean validationChiffres = false;
 
-        int nbrDeManipulation = 0;
         int position = 0;
+        int nbrDeManipulation = 0;
+        int nbrErreur = 0;
 
+        String texte = cle;
+        String texteEnMaj = "";
+        String bloc = "";
         String deuxLettres = "";
         String deuxChiffres = "";
 
-//      RG, RD, PE, PI, ou IV.
         while (valide == false) {
+            position = 0;
+            nbrErreur = 0;
             if(texte.length() % 4 == 0){
+                texteEnMaj = texte.toUpperCase();
                 nbrDeManipulation = texte.length() / 4;
                 for (int i = 0; i < nbrDeManipulation; i++) {
-                    deuxLettres = texte.substring(position , position + 1);
-                    deuxChiffres = texte.substring(position + 2 , position + 3);
+                    bloc = texteEnMaj.substring(position , position + 4);
+                    deuxLettres = bloc.substring(0,2);
+                    deuxChiffres = bloc.substring(2,4);
+                    if(deuxLettres.equals("RG") || deuxLettres.equals("RD") || deuxLettres.equals("PE") || deuxLettres.equals("PI") || deuxLettres.equals("IV")) {
+                        if(Character.isDigit(deuxChiffres.charAt(0)) && Character.isDigit(deuxChiffres.charAt(1))) {
+                            if(Integer.parseInt(deuxChiffres) >= 0 || Integer.parseInt(deuxChiffres) <= 99){
 
-                    if(deuxLettres.equals("RG")) {}
-                    position = position + 3;
+                            } else {
+                                nbrErreur++;
+                            }
+                        } else{
+                            nbrErreur++;
+                        }
+                    } else {
+                        nbrErreur++;
+                    }
+                    position = position + 4;
                 }
-                if(validationLettres == true || validationChiffres == true) {
+                if(nbrErreur == 0) {
                     valide = true;
+                } else {
+                    texte = affichageErreurCle();
                 }
-            }else{
-                System.out.print(MSG_ERREUR_CLE);
-                System.out.print(MSG_INPUT_CLE);
-                texte = Clavier.lireString();
+            } else{
+                texte = affichageErreurCle();
             }
         }
+    }
+
+    public static String affichageErreurCle() {
+        System.out.println(MSG_ERREUR_CLE);
+        System.out.print(MSG_INPUT_CLE);
+        return Clavier.lireString();
     }
 
     public static void validerChoixMenu(String texte) {
