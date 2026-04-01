63f1dd97d9bfac2e0fd11a4d66144cc86bf52fe4
William Huet <willhuet02@gmail.com>
2026-04-01 10:31:22 -0400

    FCT Classe Animal
    Classe Animal() complétée à 100% avec tous les composants nécéssaires (variables, getters, setters, constructeurs, etc...)

---
+++ laboClassesEtObjets/laboClassesEtObjets/src/Animal.java
@@ -0,0 +1,79 @@
+public class Animal {
+
+    // VARIABLES
+    private String nom;
+    private int sorte;
+    private String race;
+    private String proprietaire;
+
+    // CONSTANTES
+    // ...
+
+    //CONSTRUCTEUR PAR DÉFAUT
+    private Animal(){
+        this.nom = "";
+        this.sorte = 0;
+        this.race = "";
+        this.proprietaire = "";
+    }
+
+    // CONSTRUCTEUR AVEC PARAMÈTRES
+    public Animal(String nom, int sorte, String race, String proprietaire){
+        this.nom = nom;
+        this.sorte = sorte;
+        this.race = race;
+        this.proprietaire = proprietaire;
+    }
+
+    // GETTERS
+    public String getNom(){
+        return this.nom;
+    }
+    public int getSorte(){
+        return this.sorte;
+    }
+
+    public String getRace(){
+        return this.race;
+    }
+    public String getProprietaire(){
+        return this.proprietaire;
+    }
+
+    // SETTERS
+    public void setNom(String nom){
+        this.nom = nom;
+    }
+    public void setSorte(int sorte){
+        this.sorte = sorte;
+    }
+
+    public void setRace(String race){
+        this.race = race;
+    }
+    public void setProprietaire(String proprietaire){
+        this.proprietaire = proprietaire;
+    }
+
+    // MÉTHODES
+
+    private String typeAnimal(int chiffre){
+        String type = "";
+        if(chiffre == 0){
+            type = "Autre";
+        } else if(sorte == 1){
+            type = "Chat";
+        } else if(sorte == 2){
+            type = "Chien";
+        }
+        return type;
+    }
+
+    public void afficher(){
+        System.out.printf("%-8s: %s%n", "Nom", nom);
+        System.out.printf("%-8s: %s%n", "Sorte", typeAnimal(sorte));
+        System.out.printf("%-8s: %s%n", "Race", race);
+        System.out.printf("%-8s: %s%n", "Proprio", proprietaire);
+    }
+
+}
--- laboClassesEtObjets/laboClassesEtObjets/src/Main.java
+++ laboClassesEtObjets/laboClassesEtObjets/src/Main.java
@@ -1,6 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    Animal a1 = new Animal("Choupette", 1, "Siamois", "Louise");
+    Animal a2 = new Animal("Bill", 0, "Perruche calopsitte", "Wassim");
 
+    System.out.println();
+    a1.afficher();
 
+    System.out.println();
+    a2.afficher();
+
+    System.out.println();
+    System.out.println(a1);
 }
