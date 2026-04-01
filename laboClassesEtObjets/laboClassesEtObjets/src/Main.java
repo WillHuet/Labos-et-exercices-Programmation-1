408f8f0157e31fb7d27a2ed51b4ceb1d941696bf
William Huet <willhuet02@gmail.com>
2026-04-01 11:17:32 -0400

    FCT CarteAJouer() (exercice supplémentaire)
    Exercice supplémentaire complété (sensiblement la même chose que l'exercice principal).

--- laboClassesEtObjets/laboClassesEtObjets/src/Main.java
+++ laboClassesEtObjets/laboClassesEtObjets/src/Main.java
@@ -1,11 +1,21 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-//    Animal a1 = new Animal("Choupette", 1, "Siamois", "Louise");
-//    Animal a2 = new Animal("Bill", 0, "Perruche calopsitte", "Wassim");
-//
-//
-//    System.out.println();
-//    System.out.println(a1);
+    CarteAJouer c1 = new CarteAJouer();
+    CarteAJouer c2 = new CarteAJouer(4, "trefle");
+    CarteAJouer c3 = new CarteAJouer(1, "trèfle");
+    CarteAJouer c4 = new CarteAJouer(13, "coeur");
+
+    c1.afficher();
+    System.out.println();
+
+    c2.afficher();
+    System.out.println();
+
+    c3.afficher();
+    System.out.println();
+
+    c4.afficher();
+
 
 }
