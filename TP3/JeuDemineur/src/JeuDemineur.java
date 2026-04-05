ca5d96e83a406591a6fc4258751a171318a43c28
William Huet <willhuet02@gmail.com>
2026-04-05 16:45:03 -0400

    FCT Début de la construction de la classe JeuDemineur

--- TP3/JeuDemineur/src/JeuDemineur.java
+++ TP3/JeuDemineur/src/JeuDemineur.java
@@ -1,2 +1,73 @@
 public class JeuDemineur {
+
+    /*
+    //  ++++++++++++++
+    //  + CONSTANTES +
+    //  ++++++++++++++
+    */
+    public final char ESPACE = ' ';
+    public final char MINES = '*';
+
+    /*
+    //  ++++++++++++++++++++++++
+    //  + ATTRIBUTS D'INSTANCE +
+    //  ++++++++++++++++++++++++
+    */
+    public int nbrLignesGrille;
+    public int nbrColGrille;
+    public int nbrMines;
+    public char[][] grilleJeuCache;
+    public char[][] grilleJeuSolution;
+    public int nbrCasesSansMineDecouvertes;
+    public boolean partieGagnee = false;
+    public boolean partiePerdue = false;
+
+    /*
+    //  +++++++++++++++++++++++
+    //  + ATTRIBUTS DE CLASSE +
+    //  +++++++++++++++++++++++
+    */
+    public static int nbrPartiesJouees;
+    public static int nbrPartiesPerdues;
+
+    /*
+    //  +++++++++++++++++++++++++
+    //  + CONSTRUCTEURS PUBLICS +
+    //  +++++++++++++++++++++++++
+    */
+    public JeuDemineur(int nbrLignesGrille, int nbrColGrille, int nbrMines) {
+        this.nbrLignesGrille = validationNbrDeLignes(nbrLignesGrille);
+        this.nbrColGrille = validationNbrDeLignes(nbrColGrille);
+        this.nbrMines = validationNbrDeMines(this.nbrLignesGrille, this.nbrColGrille, nbrMines);
+    }
+
+    /*
+    //  +++++++++++++++++++++++++
+    //  + CLASSES DE VALIDATION +
+    //  +++++++++++++++++++++++++
+    */
+    public int validationNbrDeLignes(int nbrLignes){
+        int minDeLignes = 3;
+        int maxDeLignes = 8;
+        if (nbrLignes <= minDeLignes){
+            return minDeLignes;
+        } else if (nbrLignes >= maxDeLignes) {
+            return maxDeLignes;
+        } else {
+            return nbrLignes;
+        }
+    }
+
+    public int validationNbrDeMines(int nbrLignesGrille, int nbrColGrille, int nbrMines) {
+        int minDeMines = Math.max(nbrLignesGrille, nbrColGrille);
+        int maxDeMines = (nbrLignesGrille * nbrColGrille) - minDeMines;
+
+        if (nbrMines <= minDeMines) {
+            return minDeMines;
+        } else if (nbrMines >= maxDeMines) {
+            return maxDeMines;
+        } else {
+            return nbrMines;
+        }
+    }
 }
