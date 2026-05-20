c34c7d71f9ec3344f680a8dfecb6259e9c55cc79
William Huet <willhuet02@gmail.com>
2026-05-20 14:50:36 -0400

    FCT Palindrome et ajout Rectangle

--- E26/Palindrome/src/Palindrome.java
+++ E26/Palindrome/src/Palindrome.java
@@ -1,3 +1,19 @@
 public class Palindrome {
-    
+    static void main() {
+        boolean validation = true;
+        String[] tab = {"kayak", "Pip", "laval", "lol"};
+        for (String str : tab) {
+            int longueur = str.length();
+            for (int i = 0; i < longueur / 2; i++) {
+                if (str.toLowerCase().charAt(i) != str.charAt((longueur - 1) - i)) {
+                    validation = false;
+                }
+            }
+        }
+        if (validation) {
+            System.out.println("Tous les éléments du tableaux sont des palindromes");
+        } else {
+            System.out.println("Certains éléments ne sont pas des palindromes");
+        }
+    }
 }
