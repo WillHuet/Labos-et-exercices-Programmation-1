16e8b4bb6553c725802b6aca6b9acb358105278c
William <willhuet02@gmail.com>
2026-05-28 15:20:06 -0400

    ALMOST DONE
    Touche de couleur, validation Input, commentaires

--- E26/JeuTicTacToe/src/JeuTicTacToe.java
+++ E26/JeuTicTacToe/src/JeuTicTacToe.java
@@ -1,19 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class JeuTicTacToe {
     //CONSTANTES
-    private String ligneHorizontale = "---+---+---";
-    private String separateur = " | ";
-    private char letterP1 = 'X';
-    private char letterP2 = 'O';
+    private final String LIGNE_SEPARATRICE = "***********";
+    private final String LIGNE_HORIZONTALE = Colors.GREEN + "---+---+---" + Colors.RESET;
+    private final String LIGNE_VERTICALE = Colors.GREEN + " | " + Colors.RESET;
+    private final String INPUT_CASE_SELECTION = "Choisissez une case valide : ";
+    private final String ERROR_USED_CASE = Colors.FOND_ROUGE + "ERREUR! La case sélectionnée est déjà prise!" + Colors.RESET;
+    private final String ERROR_WRONG_SELECTION = Colors.FOND_ROUGE + "ERREUR! La case sélectionnée n'est pas valide (entre 1 et 9)." + Colors.RESET;
+    private final String WINNER_USER = "Vous avez gagné !!!";
+    private final String WINNER_COMPUTER = "L'ordinateur a gagné ...";
+    private final String DRAW = "Match nul ...";
+
+
+
+    private final char LETTER_P1 = 'X';
+    private final char LETTER_P2 = 'O';
+    private List<Integer> VALID_CASE_CHOICE = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
     //VARIABLES
     private Case[] tableau = new Case[9];
     private Random rand = new Random();
-    private boolean ongoingGame = true;
+    Scanner input = new Scanner(System.in);
 
     public void initializeBoard(){
         int j = 1;
@@ -36,7 +44,7 @@ public class JeuTicTacToe {
     //DÉTERMINE SI LA CASE SÉLECTIONNÉE EST DISPONIBLE OU PAS
     public boolean isCaseAvailable(Case c){
         boolean result = true;
-        if (c.getValue() == letterP1 || c.getValue() == letterP2) {
+        if (c.getValue() == LETTER_P1 || c.getValue() == LETTER_P2) {
             result = false;
         }
         return result;
@@ -45,13 +53,12 @@ public class JeuTicTacToe {
     public void computersTurn(Case[] tab, char character){
         List<Character> available = new ArrayList<>();
         for(Case c : tab){
-            if (c.getValue() != letterP1 && c.getValue() != letterP2) {
+            if (c.getValue() != LETTER_P1 && c.getValue() != LETTER_P2) {
                 available.add(c.getValue());
             }
         }
         int selectedPosition = rand.nextInt(available.size());
         char selected = available.get(selectedPosition);
-
         for(Case c : tab){
             if (c.getValue() == selected) {
                 c.setValue(character);
@@ -59,29 +66,68 @@ public class JeuTicTacToe {
         }
     }
 
+    public void playersTurn(Case[] tab, char character){
+        boolean ongoingTurn = true;
+        while (true) {
+            System.out.print(tableauToString());
+            System.out.println();
+            System.out.print(INPUT_CASE_SELECTION);
+            int position = input.nextInt();
+
+            if (VALID_CASE_CHOICE.contains(position)) {
+                if (isCaseAvailable(tab[position-1])) {
+                    tab[position-1].setValue(character);
+                    break;
+                } else {
+                    System.out.println(ERROR_USED_CASE);
+                }
+            } else {
+                System.out.println(ERROR_WRONG_SELECTION);
+            }
+        }
+    }
+
+
+    public boolean isDraw(Case[] board) {
+        boolean result = true;
+        // Vérifie s'il y a une case vide
+        for (int i = 0; i < tableau.length; i++) {
+            if (tableau[i].getValue() != LETTER_P1 && tableau[i].getValue() != LETTER_P2) {
+                result = false;
+            }
+        }
+
+        // Vérifie s'il y a un gagnant
+        if (hasWinner(board, LETTER_P1) || hasWinner(board, LETTER_P2)) {
+            result = false;
+        }
+
+        return result; // pas de case vide et pas de gagnant
+    }
+
     //VALIDE SI UNE LIGNE GAGNANTE EST SUR LE PLATEAU, RETOURNE VRAI SI C'EST LE CAS
-    public boolean isThereAWinningLine(){
+    public boolean hasWinner(Case[] board, char player) {
         boolean result = false;
         //LIGNES HORIZONTALES
-        if(tableau[0].getValue() == tableau[1].getValue() && tableau[0].getValue() == tableau[2].getValue()){
+        if(board[0].getValue() == player && board[1].getValue() == player && board[2].getValue() == player){
             result = true;
-        } else if(tableau[3].getValue() == tableau[4].getValue() && tableau[3].getValue() == tableau[5].getValue()){
+        } else if(board[3].getValue() == player && board[4].getValue() == player && board[5].getValue() == player){
             result = true;
-        } else if(tableau[6].getValue() == tableau[7].getValue() && tableau[6].getValue() == tableau[8].getValue()){
+        } else if(board[6].getValue() == player && board[7].getValue() == player && board[8].getValue() == player){
             result = true;
         }
         // LIGNES VERTICALES
-        else if(tableau[0].getValue() == tableau[3].getValue() && tableau[0].getValue() == tableau[6].getValue()){
+        else if(board[0].getValue() == player && board[3].getValue() == player && board[6].getValue() == player){
             result = true;
-        } else if(tableau[1].getValue() == tableau[4].getValue() && tableau[1].getValue() == tableau[7].getValue()){
+        } else if(board[1].getValue() == player && board[4].getValue() == player && board[7].getValue() == player){
             result = true;
-        } else if(tableau[2].getValue() == tableau[5].getValue() && tableau[2].getValue() == tableau[8].getValue()){
+        } else if(board[2].getValue() == player && board[5].getValue() == player && board[8].getValue() == player){
             result = true;
         }
         //LIGNES DIAGONALES
-        else if(tableau[0].getValue() == tableau[4].getValue() && tableau[0].getValue() == tableau[8].getValue()){
+        else if(board[0].getValue() == player && board[4].getValue() == player && board[8].getValue() == player){
             result = true;
-        } else if(tableau[2].getValue() == tableau[4].getValue() && tableau[2].getValue() == tableau[6].getValue()){
+        } else if(board[2].getValue() == player && board[4].getValue() == player && board[6].getValue() == player){
             result = true;
         }
 
@@ -90,49 +136,112 @@ public class JeuTicTacToe {
 
     public void game(){
         boolean startingPlayer = isUserStarting();
-        Scanner input = new Scanner(System.in);
+        boolean ongoingGame = true;
+
         if(startingPlayer){
             System.out.println("Vous allez jouer en premier!");
         } else {
             System.out.println("L'ordinateur jouera en premier...");
         }
 
-        do {
+        while (true){
             if(startingPlayer){
-                tableauToString();
-                System.out.print("Choisissez une case : ");
-                char position = input.next().charAt(0);
-                for (Case c : tableau) {
-                    if (c.getValue() == position) {
-                        c.setValue(letterP1);
-                    }
-                }
-                System.out.println("Maintenant l'ordinateur va jouer!");
-                computersTurn(tableau, letterP2);
-            } else {
+                playersTurn(tableau, LETTER_P1);
 
+                //verifie pour une ligne gagnante
+                if(hasWinner(tableau, LETTER_P1)) {
+                    System.out.println(tableauToString());
+                    System.out.println("JOUEUR GAGNANT!!!");
+                    break;
+                }
+
+                //verifie pour une egalite
+                if (isDraw(tableau)) {
+                    System.out.println(tableauToString());
+                    System.out.println("Match nul ...");
+                    break;
+                }
+
+                computersTurn(tableau, LETTER_P2);
+
+                //verifie pour une ligne gagnante
+                if(hasWinner(tableau, LETTER_P2)) {
+                    System.out.println(tableauToString());
+                    System.out.println("Ordinateur gagnant...");
+                    break;
+                }
+
+                //verifie pour une egalite
+                if (isDraw(tableau)) {
+                    System.out.println(tableauToString());
+                    System.out.println("Match nul ...");
+                    break;
+                }
+
+            } else {
+                computersTurn(tableau, LETTER_P1);
+
+                //verifie pour une ligne gagnante
+                if(hasWinner(tableau, LETTER_P1)) {
+                    System.out.println(tableauToString());
+                    System.out.println("Ordinateur gagnant...");
+                    break;
+                }
+
+                //verifie pour une egalite
+                if (isDraw(tableau)) {
+                    System.out.println(tableauToString());
+                    System.out.println("Match nul ...");
+                    break;
+                }
+
+                playersTurn(tableau, LETTER_P2);
+
+                //verifie pour une ligne gagnante
+                if(hasWinner(tableau, LETTER_P2)) {
+                    System.out.println(tableauToString());
+                    System.out.println("JOUEUR GAGNANT!!!");
+                    break;
+                }
+
+                //verifie pour une egalite
+                if (isDraw(tableau)) {
+                    System.out.println(tableauToString());
+                    System.out.println("Match nul ...");
+                    break;
+                }
             }
-        } while (ongoingGame);
+        }
     }
 
     public String tableauToString(){
         String grille = "";
-        grille += " " + tableau[0].getValue() + separateur + tableau[1].getValue() + separateur + tableau[2].getValue() + " \n";
-        grille += ligneHorizontale + '\n';
-        grille += " " + tableau[3].getValue() + separateur + tableau[4].getValue() + separateur + tableau[5].getValue() + " \n";
-        grille += ligneHorizontale + '\n';
-        grille += " " + tableau[6].getValue() + separateur + tableau[7].getValue() + separateur + tableau[8].getValue() + " \n";
+
+        List<String> newTableau = new ArrayList<>();
+        for(int i = 0; i < tableau.length; i++){
+            if(tableau[i].getValue() == LETTER_P1){
+                newTableau.add(Colors.RED + tableau[i].getValue() + Colors.RESET);
+            } else if(tableau[i].getValue() == LETTER_P2){
+                newTableau.add(Colors.BLUE + tableau[i].getValue() + Colors.RESET);
+            } else {
+                newTableau.add("" + tableau[i].getValue());
+            }
+        }
+
+        grille += " " + newTableau.get(0) + LIGNE_VERTICALE + newTableau.get(1) + LIGNE_VERTICALE + newTableau.get(2) + " \n";
+        grille += LIGNE_HORIZONTALE + '\n';
+        grille += " " + newTableau.get(3) + LIGNE_VERTICALE + newTableau.get(4) + LIGNE_VERTICALE + newTableau.get(5) + " \n";
+        grille += LIGNE_HORIZONTALE + '\n';
+        grille += " " + newTableau.get(6) + LIGNE_VERTICALE + newTableau.get(7) + LIGNE_VERTICALE + newTableau.get(8) + " \n";
+
         return grille;
     }
 
     void main() {
         //DÉROULEMENT DE LA PARTIE
-        System.out.println("Welcome to the Tic-Tac-Toe!");
-
+        System.out.println("Bienvenue sur le jeu TicTacToe!");
 
         initializeBoard();
-        tableau[0].setValue(letterP1);
-        System.out.println(tableauToString());
         game();
     }
 }
